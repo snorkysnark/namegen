04c7db193607f07ac8fe2f372412a11d3dfd32fd
francisthebasilisk <snorkysnark@gmail.com>
2020-08-11 23:52:58 +0300

    Added typewriter key animations

--- build/main.js
+++ build/main.js
@@ -11,27 +11,52 @@ function querySelectorErr(selector, description) {
 }
 function getTransformPosition(element) {
     const matrixString = window.getComputedStyle(element).transform;
-    const numbers = matrixString.match(/[-]?\d+/g);
+    const martix = matrixString.match(/[-]?\d+(\.\d+)?/g); //positive or negative floating point numbers
     let x = 0;
     let y = 0;
-    if (numbers && numbers.length == 6) {
-        x = +numbers[4];
-        y = +numbers[5];
+    if (martix && martix.length == 6) {
+        x = +martix[4];
+        y = +martix[5];
     }
     return { x, y };
 }
+function randomFilter(array) {
+    return array.filter((a) => Math.random() > 0.5);
+}
+function randomSort(array) {
+    return array.sort((a, b) => Math.random() - 0.5);
+}
 
+const buttonAnimationOffset = 3;
 class Typewriter {
     constructor(selector) {
+        this.buttonAnimation = null;
         let root = querySelectorErr(selector);
         this.clickArea = querySelectorErr(".typewriter-clickarea");
         this.handleTop = querySelectorErr(".tw-handle-top");
         this.handleCenter = querySelectorErr(".tw-handle-center");
         this.handleBottom = querySelectorErr(".tw-handle-bottom");
         this.buttons = [...document.querySelectorAll(".tw-button")];
+        this.initialButtonPos = getTransformPosition(this.buttons[0]);
     }
-    setOnClick(action) {
+    set onClick(action) {
         this.clickArea.addEventListener("click", action);
+    }
+    animateButtons() {
+        if (!this.buttonAnimation || this.buttonAnimation.completed) {
+            const x = this.initialButtonPos.x;
+            const y = this.initialButtonPos.y + buttonAnimationOffset;
+            const targets = randomSort(randomFilter(this.buttons));
+            const duration = 200 / targets.length;
+            this.buttonAnimation = anime({
+                targets,
+                delay: (button, index, length) => index * duration,
+                transform: `translate(${x},${y})`,
+                direction: "alternate",
+                easing: "easeOutQuad",
+                duration
+            });
+        }
     }
 }
 
@@ -58,6 +83,7 @@ class Paper {
 
 const paper = new Paper("#paper");
 const typewriter = new Typewriter("#typewriter");
-typewriter.setOnClick(() => {
+typewriter.onClick = () => {
+    typewriter.animateButtons();
     paper.pushWord("click");
-});
+};
